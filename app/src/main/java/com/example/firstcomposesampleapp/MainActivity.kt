3d96816ee5445cbc10565e8cc65fbe28239c4fce
HamidReza Farahani <h.farahani>
2023-08-23 12:59:33 +0430

    using row, image, material theme....

--- app/src/main/java/com/example/firstcomposesampleapp/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposesampleapp/MainActivity.kt
@@ -4,15 +4,27 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstcomposesampleapp.ui.theme.FirstComposeSampleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,15 +46,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(modifier: Modifier = Modifier, message: Message) {
-    Column(modifier = modifier) {
-        Text(
-            modifier = modifier.fillMaxWidth(),
-            text = "${message.author}:"
+    Row(modifier = modifier.padding(all = 8.dp)) {
+        Image(
+            modifier = modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape),
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Content profile picture"
         )
-        Text(
-            modifier = modifier.fillMaxWidth(),
-            text = message.message
-        )
+
+        Spacer(modifier = modifier.width(8.dp))
+
+        Column(modifier = modifier) {
+            Text(
+                modifier = modifier.fillMaxWidth(),
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall,
+                text = "${message.author}:"
+            )
+
+            Spacer(modifier = modifier.height(4.dp))
+
+            Text(
+                modifier = modifier.fillMaxWidth(),
+                text = message.message
+            )
+        }
     }
 }
 
